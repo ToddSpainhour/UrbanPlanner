c68f60b3ceab925b42067b092c9eed5e36aa129c
Todd Spainhour <toddspainhour@gmail.com>
2020-08-09 23:00:36 -0500

    assignment complete

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrbanPlanner
+{
+    class Building
+    {
+
+   
+
+        //fields
+
+        private string _designer = "Todd Spainhour";
+        private DateTime _dateConstructed;
+        private string _address = "";
+        private string _owner = "";
+
+
+
+
+        //properties
+
+        public int Stories = 3;
+        public double Width;
+        public double Depth;
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+
+
+
+        //constructors
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+
+
+        //methods
+
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+
+
+        public void Purchase(string _owner)
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine($"Address: {_address}");
+            Console.WriteLine($"Designed by: {_designer}");
+            Console.WriteLine($"Constructed on: {_dateConstructed}");
+            Console.WriteLine($"Owned by: {_owner}");
+            Console.WriteLine($"Cubic Meters: {Volume}");
+            Console.WriteLine("\n");
+        }
+    }
+}
--- UrbanPlanner/Program.cs
+++ UrbanPlanner/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection.Emit;
+using UrbanPlanner;
 
 namespace UrbanPlanner
 {
@@ -7,6 +9,30 @@ namespace UrbanPlanner
         static void Main(string[] args)
         {
             Console.WriteLine("--Urban Planner Assignment--");
+            Console.WriteLine("\n");
+
+            Building myFirstProperty = new Building("123 Main Street");
+            myFirstProperty.Construct();
+            myFirstProperty.Stories = 5;
+            myFirstProperty.Width = 30;
+            myFirstProperty.Depth = 25;
+            myFirstProperty.Purchase("Jimbo Real Estate Holdings");
+
+
+            Building mySecondProperty = new Building("456 West Bridge Road");
+            mySecondProperty.Construct();
+            mySecondProperty.Stories = 3;
+            mySecondProperty.Width = 15;
+            mySecondProperty.Depth = 20;
+            mySecondProperty.Purchase("Mr. and Mrs. Smith");
+
+
+            Building myThirdProperty = new Building("789 Scout Way");
+            myThirdProperty.Construct();
+            myThirdProperty.Stories = 10;
+            myThirdProperty.Width = 150;
+            myThirdProperty.Depth = 200;
+            myThirdProperty.Purchase("VS Group, LLC.");
         }
     }
 }
